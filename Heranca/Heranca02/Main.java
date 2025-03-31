000da9eb9d05b8212efc6ad795b32594fb6f0748
IsaacSouzaaa <168234393+IsaacSouzaaa@users.noreply.github.com>
2025-03-31 20:58:24 -0300

    tentando corrigir

---
+++ Heranca/Heranca02/Main.java
@@ -0,0 +1,10 @@
+package Heranca02;
+
+public class Main {
+    public static void main(String[] args) {
+        Veiculo[] veiculos = {new Veiculo(), new Bicicleta()};
+        for (Veiculo v : veiculos) {
+            v.mover();
+        }
+    }
+}
